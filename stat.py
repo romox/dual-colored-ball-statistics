d1eac464200cd0956d13ea2849b5e856a969020f
Chris Yang <719417303@qq.com>
2014-07-14 18:57:09 +0800

    v0.2

--- stat.py
+++ stat.py
@@ -9,44 +9,53 @@ def open_excel(file= 'file.xls'):
         print str(e)
 
 #根据名称获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_name：Sheet1名称
-def excel_table_byname(file= 'file.xls',index=20,by_name=u'Sheet1'):
+def excel_table_byname(file,by_name,index,row_count):
     data = open_excel(file)
     table = data.sheet_by_name(by_name)
     data =  table.col_values(index) #某一行数据 
     data.pop(0)
     data = [x for x in data if x != '']
-    return data
+    return data[-row_count:]
 
-def occurrences(string, sub):
+def occurrences(string, sub, input_count):
     result = []
     count = start = 0
     while True:
         start = string.find(sub, start) + 1
         if start > 0:
             count+=1
-            if start-1+4 < len(string):
-                result.append(int(string[string.index(sub,start-1)+4]))
+            if start-1+input_count < len(string):
+                result.append(int(string[string.index(sub,start-1)+input_count]))
         else:
             return (count, result)
 
 
 def main():
 
-   col = input('请输入列数: ')
-   # print col
-   data = excel_table_byname(index=col)
+   col_count = input('请输入列数: ')
+   row_count = input('请输入行数: ')
+   # print col_count
+   data = excel_table_byname(file='file.xls', by_name=u'Sheet1', index=col_count,row_count=row_count)
 
    data = [str(int(i)) for i in data]
    datastring = ''.join(data)
-
-   var1,var2,var3,var4 = raw_input("请输入一个序列(4个0到2的数，以空格隔开): ").split()
-   inputstring = var1+var2+var3+var4
+   
+   input_count = input("请输入3或4: ")
+   inputstring = ''
+   if input_count == 3:
+     var1,var2,var3 = raw_input("请输入一个序列(%d个0到2的数，以空格隔开): " % input_count).split()
+     inputstring = var1+var2+var3
+   elif input_count == 4:
+     var1,var2,var3,var4 = raw_input("请输入一个序列(4个0到2的数，以空格隔开): ").split()
+     inputstring = var1+var2+var3+var4
+   else:
+     assert inputstring, '输入错误，请重新输入'
 
    # print inputstring
    # print datastring
    # print len(datastring)
 
-   count,result =  occurrences(datastring,inputstring)
+   count,result =  occurrences(datastring,inputstring,input_count)
 
    zero = one = two = 0
 
@@ -62,7 +71,7 @@ def main():
 
    # print counts
    firstline = '在第%d列中' + inputstring + '一共出现了%d次'
-   print firstline % (col, count)
+   print firstline % (col_count, count)
    print '0出现了%(zero)s次' % counts
    print '1出现了%(one)s次' % counts
    print '2出现了%(two)s次' % counts
