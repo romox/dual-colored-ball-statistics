a707fee20fb1d1fa429b8883b9f6876965a4faea
Chris Yang <junqi.yang.adp@gmail.com>
2014-07-27 16:27:05 +0800

    infinite loop

--- stat.py
+++ stat.py
@@ -31,49 +31,51 @@ def occurrences(string, sub, input_count):
 
 
 def main():
-   name = raw_input('请输入文件名：')
-   name = name + '.xls'
-   col_count = input('请输入列数: ')
-   row_count = input('请输入行数: ')
-   # print col_count
-   data = excel_table_byname(file=name, by_name=u'Sheet1', index=col_count,row_count=row_count)
 
-   data = [str(int(i)) for i in data]
-   datastring = ''.join(data)
-   
-   input_count = input("请输入3或4: ")
-   inputstring = ''
-   if input_count == 3:
-     inputstring = raw_input("请输入一个序列(3个0到2的数，以空格隔开): ")
-   elif input_count == 4:
-     inputstring = raw_input("请输入一个序列(4个0到2的数，以空格隔开): ")
-   else:
-     assert inputstring, '输入错误，请重新输入'
+   while True:
+           name = raw_input('请输入文件名：')
+           name = name + '.xls'
+           col_count = input('请输入列数: ')
+           row_count = input('请输入行数: ')
+           # print col_count
+           data = excel_table_byname(file=name, by_name=u'Sheet1', index=col_count,row_count=row_count)
 
-   # print inputstring
-   # print datastring
-   # print len(datastring)
+           data = [str(int(i)) for i in data]
+           datastring = ''.join(data)
+           
+           input_count = input("请输入3或4: ")
+           inputstring = ''
+           if input_count == 3:
+             inputstring = raw_input("请输入一个序列(3个0到2的数，以空格隔开): ")
+           elif input_count == 4:
+             inputstring = raw_input("请输入一个序列(4个0到2的数，以空格隔开): ")
+           else:
+             assert inputstring, '输入错误，请重新输入'
 
-   count,result =  occurrences(datastring,inputstring,input_count)
+           # print inputstring
+           # print datastring
+           # print len(datastring)
 
-   zero = one = two = 0
+           count,result =  occurrences(datastring,inputstring,input_count)
 
-   for x in result:
-    if x == 0:
-      zero += 1 
-    if x == 1:
-      one += 1
-    if x == 2:
-      two += 1
+           zero = one = two = 0
 
-   counts = {'zero': zero, 'one': one, 'two': two}
+           for x in result:
+            if x == 0:
+              zero += 1 
+            if x == 1:
+              one += 1
+            if x == 2:
+              two += 1
 
-   # print counts
-   firstline = '在第%d列中' + inputstring + '一共出现了%d次'
-   print firstline % (col_count, count)
-   print '0出现了%(zero)s次' % counts
-   print '1出现了%(one)s次' % counts
-   print '2出现了%(two)s次' % counts
+           counts = {'zero': zero, 'one': one, 'two': two}
+
+           # print counts
+           firstline = '在第%d列中' + inputstring + '一共出现了%d次'
+           print firstline % (col_count, count)
+           print '0出现了%(zero)s次' % counts
+           print '1出现了%(one)s次' % counts
+           print '2出现了%(two)s次' % counts
 
 if __name__=="__main__":
     main()
