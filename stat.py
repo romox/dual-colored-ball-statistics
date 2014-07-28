4a694c74bf4966f31bca1eed5dcda2c2ea6cb0d4
Chris Yang <junqi.yang.adp@gmail.com>
2014-07-28 17:15:15 +0800

    stat and stat2 combined

--- stat.py
+++ stat.py
@@ -43,39 +43,61 @@ def main():
            data = [str(int(i)) for i in data]
            datastring = ''.join(data)
            
-           input_count = input("请输入3或4: ")
+           input_count = input("请输入2或3或4: ")
            inputstring = ''
-           if input_count == 3:
-             inputstring = raw_input("请输入一个序列(3个0到2的数，以空格隔开): ")
-           elif input_count == 4:
-             inputstring = raw_input("请输入一个序列(4个0到2的数，以空格隔开): ")
+           if input_count == 2 or input_count == 3 or input_count == 4:
+             inputstring = raw_input("请输入一个序列: ")
            else:
              assert inputstring, '输入错误，请重新输入'
 
-           # print inputstring
-           # print datastring
-           # print len(datastring)
+##           print inputstring
+##           print datastring
+##           print len(datastring)
 
            count,result =  occurrences(datastring,inputstring,input_count)
 
-           zero = one = two = 0
+           zero = one = two = three = four = five = six = seven = eight = nine = 0
 
            for x in result:
             if x == 0:
               zero += 1 
-            if x == 1:
+            elif x == 1:
               one += 1
-            if x == 2:
+            elif x == 2:
               two += 1
+            elif x == 3:
+              three += 1 
+            elif x == 4:
+              four += 1
+            elif x == 5:
+              five += 1
+            elif x == 6:
+              six += 1
+            elif x == 7:
+              seven += 1
+            elif x == 8:
+              eight += 1
+            elif x == 9:
+              nine += 1  
 
-           counts = {'zero': zero, 'one': one, 'two': two}
 
-           # print counts
+           counts = {'zero': zero, 'one': one, 'two': two, 'three': three,
+                     'four': four, 'five': five, 'six': six, 'seven': seven,
+                     'eight': eight, 'nine': nine}
+
+##           print counts
            firstline = '在第%d列中' + inputstring + '一共出现了%d次'
            print firstline % (col_count, count)
            print '0出现了%(zero)s次' % counts
            print '1出现了%(one)s次' % counts
            print '2出现了%(two)s次' % counts
+           print '3出现了%(three)s次' % counts
+           print '4出现了%(four)s次' % counts
+           print '5出现了%(five)s次' % counts
+           print '6出现了%(six)s次' % counts
+           print '7出现了%(seven)s次' % counts
+           print '8出现了%(eight)s次' % counts
+           print '9出现了%(nine)s次' % counts
 
 if __name__=="__main__":
     main()
